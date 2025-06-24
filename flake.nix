b7719d944384377030169dbceb23febbf5dd487e
Jack D. Douglas <cincomc@proton.me>
2025-06-24 20:24:59 +0100

    add support for laptop hostname

--- flake.nix
+++ flake.nix
@@ -271,7 +271,38 @@
         ];
       };
 
+      darwinConfigurations."laptop" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jackdouglas = import ./home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "jackdouglas";
+
+              # Automatically migrate existing Homebrew installations
+              # autoMigrate = true;
+            };
+          }
+        ];
+      };
+
       # Expose the package set, including overlays, for convenience.
-      darwinPackages = self.darwinConfigurations."jack-tonk".pkgs;
+      darwinPackages = builtins.mapAttrs (name: config: config.pkgs) self.darwinConfigurations;
     };
 }
