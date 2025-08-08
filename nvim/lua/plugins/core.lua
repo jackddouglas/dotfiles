bc59e896ee626eb6b0c065f88cb6cdbf4a1ad31d
Jack D. Douglas <cincomc@proton.me>
2025-08-08 22:53:36 +0100

    nvim: clean up core

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -6,35 +6,6 @@ return {
     },
   },
   {
-    "folke/snacks.nvim",
-    opts = {
-      explorer = {
-        -- your explorer configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
-      picker = {
-        sources = {
-          explorer = {
-            -- your explorer picker configuration comes here
-            -- or leave it empty to use the default settings
-            auto_close = true,
-          },
-        },
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        hls = {
-          mason = false,
-        },
-      },
-    },
-  },
-  {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
