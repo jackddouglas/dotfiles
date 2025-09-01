2f2cd47069bad3c6f5443506a7aaf1ebd5c4813b
Jack D. Douglas <cincomc@proton.me>
2025-09-01 09:44:36 +0100

    nvim: telescope prompt at top

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -39,6 +39,10 @@ return {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+          prompt_position = "top",
+        },
         preview = {
           treesitter = {
             enable = true,
