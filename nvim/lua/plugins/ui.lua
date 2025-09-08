aa0829c243d9d3869cf27065619c3b6f2e084a9e
Jack D. Douglas <cincomc@proton.me>
2025-09-08 10:33:07 +0100

    nvim: ignore spacing buffers with tmux

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,3 +1,20 @@
+local TmuxNavigateIgnore = function(motion, buftype)
+  vim.g.last_pane = vim.fn.win_getid()
+  vim.cmd(motion)
+  if vim.bo.buftype ~= buftype then
+    vim.g.last_pane = vim.fn.win_getid()
+  else
+    while vim.bo.buftype == buftype do
+      local jumped_to = vim.fn.win_getid()
+      vim.cmd(motion)
+      if vim.fn.win_getid() == jumped_to then
+        vim.fn.win_gotoid(vim.g.last_pane)
+        break
+      end
+    end
+  end
+end
+
 return {
   {
     "shortcuts/no-neck-pain.nvim",
@@ -6,6 +23,7 @@ return {
       width = 150,
       autocmds = {
         enableOnVimEnter = "safe",
+        skipEnteringNoNeckPainBuffer = true,
       },
       integrations = {
         dashboard = {
@@ -23,5 +41,52 @@ return {
         scratchPad = "<leader>uns",
       },
     },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      {
+        "<c-h>",
+        function()
+          TmuxNavigateIgnore("TmuxNavigateLeft", "nofile")
+        end,
+      },
+      {
+        "<c-j>",
+        function()
+          TmuxNavigateIgnore("TmuxNavigateDown", "nofile")
+        end,
+      },
+      {
+        "<c-k>",
+        function()
+          TmuxNavigateIgnore("TmuxNavigateUp", "nofile")
+        end,
+      },
+      {
+        "<c-l>",
+        function()
+          TmuxNavigateIgnore("TmuxNavigateRight", "nofile")
+        end,
+      },
+    },
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      -- These autocommands will focus on the last buffer that was not ignored, when we go from a tmux pane to a vim split
+      vim.api.nvim_create_autocmd({ "VimEnter", "FocusGained", "TabEnter" }, {
+        pattern = { "*" },
+        callback = function()
+          vim.fn.win_gotoid(vim.g.last_pane)
+        end,
+      })
+    end,
   },
 }
--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -11,24 +11,6 @@ return {
     end,
   },
   {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-  {
     "norcalli/nvim-colorizer.lua",
     config = function()
       require("colorizer").setup({
