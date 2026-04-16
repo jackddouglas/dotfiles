f9f53e0bb4821bd1522011521b0d219184c311db
Jack D. Douglas <cincomc@proton.me>
2026-04-16 09:37:38 +0100

    revert kanata script to raycast

--- scripts/restart-kanata.sh
+++ scripts/restart-kanata.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# @tuna.name Restart Kanata
-# @tuna.subtitle Unload and reload kanata daemon
-# @tuna.mode inline
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Kanata
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⌨️
+
+# Documentation:
+# @raycast.author Jack D. Douglas
 
 sudo launchctl unload /Library/LaunchDaemons/com.jackdouglas.kanata.plist
 sudo launchctl load /Library/LaunchDaemons/com.jackdouglas.kanata.plist
